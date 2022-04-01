ac5dedd6f5c2206408bc183c2469865492f30a18
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-04-01 01:54:01 -0400

    feat: sudoku solver

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ window.onload = () => {
   const DOM = {
     FORM: document.getElementById("solver"),
     INPUT: document.getElementById("boardInput"),
+    CELLS: new Array(9).fill().map(() => new Array(9)),
+    SUBMIT_BTN: document.getElementById("submitBtn"),
   };
 
   const BOARD = new Array(9).fill().map(() => new Uint8Array(9));
@@ -17,6 +19,7 @@ window.onload = () => {
       input.setAttribute("data-col_i", colI);
 
       DOM.INPUT.append(input);
+      DOM.CELLS[rowI][colI] = input;
     }
   }
 
@@ -27,6 +30,83 @@ window.onload = () => {
 
   DOM.FORM.onsubmit = (e) => {
     e.preventDefault();
-    console.log(BOARD);
+    DOM.SUBMIT_BTN.disabled = true;
+
+    const start = performance.now();
+    const solved = solveSudoku(BOARD);
+    console.log(performance.now() - start);
+
+    if (solved) {
+      for (let rowI = 0; rowI < 9; rowI++) {
+        for (let colI = 0; colI < 9; colI++) {
+          const cell = DOM.CELLS[rowI][colI];
+          if (!cell.value) {
+            cell.value = solved[rowI][colI];
+            cell.setAttribute("class", "solved");
+          }
+          cell.disabled = true;
+        }
+      }
+      DOM.SUBMIT_BTN.innerText = "Solved!";
+    } else {
+      DOM.SUBMIT_BTN.disabled = false;
+      DOM.SUBMIT_BTN.innerText = "Unsolvable board";
+    }
   };
+};
+
+const isValidSudoku = (board) => {
+  const isValidUnit = (items) => {
+    const hashMap = {};
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (item !== 0 && hashMap[item]) {
+        return false;
+      } else {
+        hashMap[item] = true;
+      }
+    }
+
+    return true;
+  };
+
+  for (let i = 0; i < 9; i++) {
+    const row = Array(9);
+    const col = Array(9);
+    const box = Array(9);
+
+    for (let j = 0; j < 9; j++) {
+      row[j] = board[i][j];
+      col[j] = board[j][i];
+      box[j] = board[i - (i % 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
+    }
+
+    if (!isValidUnit(row) || !isValidUnit(col) || !isValidUnit(box))
+      return false;
+  }
+
+  return true;
+};
+
+const isCompleteSudoku = (board) =>
+  board.every((row) => row.every((cell) => cell !== 0));
+
+const solveSudoku = (board) => {
+  if (!isValidSudoku(board)) return null;
+
+  for (let rowI = 0; rowI < 9; rowI++) {
+    for (let colI = 0; colI < 9; colI++) {
+      if (board[rowI][colI] === 0) {
+        for (let candidate = 1; candidate < 10; candidate++) {
+          board[rowI][colI] = candidate;
+          solveSudoku(board);
+          if (isCompleteSudoku(board) && isValidSudoku(board)) return board;
+        }
+        board[rowI][colI] = 0;
+        return null;
+      }
+    }
+  }
+
+  return null;
 };
